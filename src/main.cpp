4eafee1c8ec66386a54925b3063dd3756e7ae422
DucksFoLif <justinlb3003@gmail.com>
2023-04-29 16:45:14 -0700

    INTRIO

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,11 @@
 #include "Application.h"
-#include "Intro.h""
+#include "Intro.h"
 
 #include <iostream>
 
 int main()
 {
-    //Intro start;
+    Intro start;
 
     Application app{ 1280, 720, 4, "Viking Quest: The Green Crusade" };
 
